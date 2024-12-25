3f0bc19a3e6b73eced6504df284154e9f62f0786
Restrained <958265107@qq.com>
2024-12-25 11:13:46 +0800

    调度器修改redis配置

--- scheduler/celery_config.py
+++ scheduler/celery_config.py
@@ -12,14 +12,15 @@ from celery import Celery
 from celery.schedules import crontab
 
 
-# 导入任务模块
-from tasks import run_spider  # 确保这个路径正确
 
 # 初始化 Celery 实例，指定任务队列的 broker（使用 Redis）
 app = Celery('spider', broker='redis://:Hzxc2023@192.168.0.41:16379/9')
 
 # 配置 Celery 的结果存储（可以使用 Redis 或其他存储）
 app.conf.result_backend = 'redis://localhost:6379/0'
+
+# 自动发现任务模块中的任务
+app.autodiscover_tasks(['tasks'])
 
 # 配置定时任务，每分钟执行一次任务
 app.conf.beat_schedule = {
